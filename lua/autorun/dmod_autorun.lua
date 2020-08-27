07c9fd2aa88a89b7a65895a6bd7881149e967d0a
Rex69420 <70341792+Rex6969@users.noreply.github.com>
2020-08-28 01:27:49 +0700

    Create dmod_autorun.lua

---
+++ lua/autorun/dmod_autorun.lua
@@ -0,0 +1,9 @@
+if !CPTBase then print("CPTBase addon is missing!") return end
+
+CPTBase.RegisterMod("DMod I","in development")
+
+local Category = "DOOM"
+
+CPTBase.AddNPC("Imp","npc_dmod_imp2016", Category)
+--CPTBase.AddNPC("Imp (DOOM Eternal)","npc_dmod_imp_eternal", Category)
+
