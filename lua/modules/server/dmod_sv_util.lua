bce789b8049c02016a3091e5968823eddd84dd65
Rex69420 <70341792+Rex6969@users.noreply.github.com>
2020-09-18 18:41:48 +0700

    Update dmod_sv_util.lua

--- lua/modules/server/dmod_sv_util.lua
+++ lua/modules/server/dmod_sv_util.lua
@@ -1,14 +1,24 @@
--- Purpose: imp's and possessed soldier's movement code.
+-- Purpose: imp's and possessed soldier's movement code. Picks a random position between the _min and _max aroung the entity 'ent'
 
-function ENT:RecomputeSurroundPath( argent, _min, _max )
+function ENT:RecomputeSurroundPath( ent, _min, _max )
+	
 	local finpos, tries, maxtries = false, 0, 10
-		while tries < maxtries do 
-			local _pos = self:RX_RandomPos( argent, _min,  _max )
-			if !util.IsInWorld(pos) and argent:VisibleVec(_pos+Vector(0,0,60)) and !self:IsInRange(_pos, 300) then
-				finpos = _pos
-				break
-			end
-			tries = tries + 1
+	
+	while tries < maxtries do 
+		local _pos = self:RX_RandomPos( ent, _min,  _max )
+		if !util.IsInWorld(_pos) and ent:VisibleVec(_pos+Vector(0,0,60)) and !self:IsInRange(_pos, 300) then
+			finpos = _pos
+			break
 		end
-		return finpos
+		tries = tries + 1
 	end
+	return finpos
+end
+
+function ENT:GetMovementTarget()
+	return self.MovementTarget or false
+end
+
+function ENT:SetMovementTarget(pos)
+	self.MovementTarget = pos or false
+end
